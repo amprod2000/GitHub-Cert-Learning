9b2ebae36152ddc5bfe0e28fc2b552c6d5a3bb84
CODE33301 <code33301@gmail.com>
2024-10-14 17:25:40 -0500

    Added a terraform file main.tf & updated notes

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 # Configure the GitHub Provider
 provider "github" {}
 
-data "github_branch" "development" {
+resource "github_branch" "development" {
   repository = "GitHub-Cert-Learning"
   branch     = "sdks"
 }
